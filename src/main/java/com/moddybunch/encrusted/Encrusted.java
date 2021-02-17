9b5e1268bd8a2c1b964c3da35236eca660224c5b
loganwhaley <61551153+loganwhaley@users.noreply.github.com>
2021-02-16 19:15:42 -0500

    Failure

--- src/main/java/com/moddybunch/encrusted/Encrusted.java
+++ src/main/java/com/moddybunch/encrusted/Encrusted.java
@@ -2,14 +2,40 @@ package com.moddybunch.encrusted;
 
 import com.moddybunch.encrusted.util.EncrustedRegistries;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.util.Identifier;
 
 public class Encrusted implements ModInitializer {
 
 	// Our modid, reference this instead of actually typing "encrusted"
 	public static final String MODID = "encrusted";
 
+	// Creates the new chest loot tables
+	private static final Identifier Ruined_Portal_Loot_Table_ID = new Identifier("minecraft", "chests/ruined_portal");
+
 	@Override
 	public void onInitialize() {
+
 		EncrustedRegistries.init();
+		modifyLootTables();
+	}
+
+	private void modifyLootTables() {
+		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+			// Checks for the ruined portal chest loot table
+			if (Ruined_Portal_Loot_Table_ID.equals(id)){
+				// Adds the ruby to the loot table
+				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+						.rolls(ConstantLootTableRange.create(1))
+						.with(LootTableEntry.builder(Ruined_Portal_Loot_Table_ID));
+				supplier.withPool(poolBuilder.build());
+			}
+
+
+
+		} );
 	}
 }
